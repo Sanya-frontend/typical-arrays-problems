4ca72735360f985eb55e5bb568cc198e1b6979a5
Sanya-frontend <79463917+Sanya-frontend@users.noreply.github.com>
2021-03-14 22:18:04 +0300

    fix: change function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
-
 exports.min = function min (array) {
-  return 0;
+  if(!array || array.length==0) {
+    return 0;
+  }
+   else
+    return Math.min.apply(0, array);
 }
+
 
 exports.max = function max (array) {
-  return 0;
+  if(!array || array.length==0) {
+    return 0;
+}
+ else
+    return Math.max.apply(0, array);
 }
 
-exports.avg = function avg (array) {
-  return 0;
+
+exports.avg = function avg(array) {
+  let number = 0;
+  if(!array || array.length==0) {
+     return 0;
+} 
+ else
+    for (let i=0; i<array.length; i++) {
+      number = number + array[i];
+}
+    return number/array.length;
 }
